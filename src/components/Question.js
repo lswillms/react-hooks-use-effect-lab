4ac2a5a7018319e4728877677944cca7f274cc86
Latoshia Williams <lswillms@yahoo.com>
2022-11-14 19:57:08 -0600

    commit tree 2b2d6186415f9712b95d5e3f835d7abe890434d6

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    const resetTimer = timeRemaining > 0 && setTimeout(() => { 
+      setTimeRemaining(timeRemaining -1 )}, 1000)
+      
+      useEffect(() => {
+        if (timeRemaining === 0) {
+          setTimeRemaining(10);
+          onAnswered(false);
+          return; // exit early!
+        })
+
+    return function cleanUp() {
+      clearTimeout(resetTimer, resetQuestion)
+    }
+  },[timeRemaining] )
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
